5b7d129844b5eedff7677ffab99954fc2f64bd88
FullstackDevelopment <ankitkr756208@gmail.com>
2026-09-20 10:23:38 +0530

    updated code

---
+++ JAVASCRIPT_11/practice.js
@@ -0,0 +1,16 @@
+// var a=8;
+// console.log(a);
+
+// const pers1={
+//     Name : 'Ankit kumar',
+//     age: 23,
+//     gmail:"AK@gmail.com",
+//     address:'Patna',
+//     country:'india'
+// }
+// console.log(pers1)
+
+// const pers2={...pers1,Name:'Aman',age:24}
+// console.log(pers2)
+
+ 
---
+++ Shoping-app/client/src/Pages/Userlayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
+import Home from '../components/Home'
+import Footer from '../components/Footer'
+import { Outlet } from 'react-router-dom'
+
+const Userlayout = () => {
+  return (
+    <div>
+        <Header/>
+        <Navbar/>
+         <Outlet/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Userlayout
